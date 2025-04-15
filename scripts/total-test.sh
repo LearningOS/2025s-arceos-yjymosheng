9c443b6cffb811e416bf2df67d0a0411daf71e46
yjymosheng <yjymosheng@gmail.com>
2025-04-15 10:30:42 +0800

    update lab1

--- scripts/total-test.sh
+++ scripts/total-test.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
-
+file_name=test.output
 score=0
+
+rm -f $file_name
+
+
+
+touch  $file_name
 
 if ./scripts/test-print.sh ; then
     ((score += 100))
 else
-    echo "test-print failed"
+    echo "test-print failed" >> $file_name
 fi
 
 if ./scripts/test-ramfs_rename.sh ; then
     ((score += 100))
 else
-    echo "test-ramfs_rename failed"
+    echo "test-ramfs_rename failed" >> $file_name
 fi
 
 if ./scripts/test-alt_alloc.sh ; then
     ((score += 100))
 else
-    echo "test-alt_alloc failed"
+    echo "test-alt_alloc failed" >> $file_name
 fi
 
 if ./scripts/test-support_hashmap.sh ; then
     ((score += 100))
 else
-    echo "test-support_hashmap failed"
+    echo "test-support_hashmap failed" >> $file_name
 fi
 
 if ./scripts/test-sys_map.sh ; then
     ((score += 100))
 else
-    echo "test-sys_map failed"
+    echo "test-sys_map failed" >> $file_name
 fi
 
 if ./scripts/test-simple_hv.sh ; then
     ((score += 100))
 else
-    echo "test-simple_hv failed"
+    echo "test-simple_hv failed" >> $file_name
 fi
 
 echo "$score"
